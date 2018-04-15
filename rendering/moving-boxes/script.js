163f475d2e795e8669bbc8736c38a06e2c7c5a7b
Patryk Karny <patryk.karny@infullmobile.com>
2018-04-15 23:04:00 +0200

    Exercise nine - optimize animation function with requestAnimationFrame

--- rendering/moving-boxes/script.js
+++ rendering/moving-boxes/script.js
@@ -5,16 +5,12 @@ const {
 
  const elements = Array.from(document.querySelectorAll('.element'));
 
-registerNextClick(function (timestamp) {
-  const nextPositions = elements.map(element => {
+ registerNextClick(function (timestamp) {
+  elements.forEach((element, index) => {
     const top = element.offsetTop;
     const nextPosition = +((Math.sin(top + timestamp/1000) + 1) / 2 * containerWidth);
-
-    return nextPosition;
-  });
-
-  elements.forEach((element, index) => {
-    const nextPosition = nextPositions[index];
-    element.style.transform = `translateX(${nextPosition}px)`;
+    requestAnimationFrame(() => {
+      element.style.transform = `translateX(${nextPosition}px)`;
+    });
   });
  });
