04d69728248cde3ccfdab67fce452b96fb8e3253
Patryk Karny <patryk.karny@infullmobile.com>
2018-04-15 22:59:21 +0200

    Exercise seven - have same map

--- benchmarks/classes.js
+++ benchmarks/classes.js
@@ -1,4 +1,20 @@
-const a = { a: 1 };
-const b = { b: 1 };
+// const a = { a: 1, x: 10 };
+// const b = { a: 5000 };
+
+// b.x = 10;
+
+const makeAPoint = () => {
+  class Point {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+
+  return new Point(1, 2);
+}
+
+const a = makeAPoint();
+const b = makeAPoint();
 
 console.log(%HaveSameMap(a, b));
