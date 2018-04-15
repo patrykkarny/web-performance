4d7d48f284e30c48e6a41d4c737bd471ebe27021
Patryk Karny <patryk.karny@infullmobile.com>
2018-04-15 23:07:30 +0200

    Exercise twelve - optimize transform with will change prop

--- rendering/paint-storming/script.js
+++ rendering/paint-storming/script.js
@@ -1,7 +1,23 @@
-const $box = $('.box');
+const box = document.querySelector('.box');
 
-$box.on('click', () => {
-  $box.animate({
-    marginLeft: '500px',
-  }, 500);
+box.addEventListener('mouseenter', () => {
+  box.style.willChange = 'transform';
 });
+
+box.addEventListener('mouseleave', () => {
+  box.style.willChange = 'auto';
+});
+
+box.addEventListener('click', () => {
+  box.classList.toggle('move');
+});
+
+box.addEventListener('transitionend', () => {
+  box.style.willChange = 'auto';
+})
+
+// $box.on('click', () => {
+//   $box.animate({
+//     marginLeft: '500px',
+//   }, 500);
+// });
