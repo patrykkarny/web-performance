3f093ea29ae4e37e92a31e68112e8fc806ac09bf
Patryk Karny <patryk.karny@infullmobile.com>
2018-04-15 23:04:37 +0200

    Exercise ten - optimize animation function with fastDOM library

--- rendering/moving-boxes/script.js
+++ rendering/moving-boxes/script.js
@@ -5,12 +5,15 @@ const {
 
  const elements = Array.from(document.querySelectorAll('.element'));
 
- registerNextClick(function (timestamp) {
+registerNextClick(function (timestamp) {
   elements.forEach((element, index) => {
-    const top = element.offsetTop;
-    const nextPosition = +((Math.sin(top + timestamp/1000) + 1) / 2 * containerWidth);
-    requestAnimationFrame(() => {
-      element.style.transform = `translateX(${nextPosition}px)`;
+    fastdom.measure(() => {
+      const top = element.offsetTop;
+      const nextPosition = +((Math.sin(top + timestamp/1000) + 1) / 2 * containerWidth);
+      fastdom.mutate(() => {
+        element.style.transform = `translateX(${nextPosition}px)`;
+      });
     });
   });
  });
+ 
