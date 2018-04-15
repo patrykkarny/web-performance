0cdae658754ab8f34a17f4e3cf0b2705141f174a
Patryk Karny <patryk.karny@infullmobile.com>
2018-04-15 22:48:24 +0200

    Exercise one - never optimize function

--- benchmarks/benchmark.js
+++ benchmarks/benchmark.js
@@ -2,7 +2,8 @@ const { performance } = require('perf_hooks');
 
 // SETUP 🏁
 
-let iterations = 1e7;
+// let iterations = 1e7;
+let iterations = 100000;
 
 const a = 1;
 const b = 2;
@@ -13,7 +14,17 @@ const add = (x, y) => x + y;
 
 performance.mark('start');
 
+%NeverOptimizeFunction(add);
+
 // EXERCISE 💪
+
+while (iterations--) {
+  add(a, b);
+}
+
+iterations = 1e7;
+
+add('foo', 'bar');
 
 while (iterations--) {
   add(a, b);
@@ -27,3 +38,8 @@ performance.measure('My Special Benchmark', 'start', 'end');
 
 const [ measure ] = performance.getEntriesByName('My Special Benchmark');
 console.log(measure);
+
+// node benchmarks/benchmark.js
+// node --trace-opt benchmarks/benchmark.js | grep add
+// node --trace-opt --trace-deopt benchmarks/benchmark.js | grep add
+// node --allow-natives-syntax benchmarks/benchmark.js
