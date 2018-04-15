fec281cf31276180e2ed59d9f23349b77f15679f
Patryk Karny <patryk.karny@infullmobile.com>
2018-04-15 23:03:21 +0200

    Exercise eight - optimize animation function

--- rendering/moving-boxes/script.js
+++ rendering/moving-boxes/script.js
@@ -5,10 +5,16 @@ const {
 
  const elements = Array.from(document.querySelectorAll('.element'));
 
- registerNextClick(function (timestamp) {
-  elements.forEach((element, index) => {
+registerNextClick(function (timestamp) {
+  const nextPositions = elements.map(element => {
     const top = element.offsetTop;
     const nextPosition = +((Math.sin(top + timestamp/1000) + 1) / 2 * containerWidth);
+
+    return nextPosition;
+  });
+
+  elements.forEach((element, index) => {
+    const nextPosition = nextPositions[index];
     element.style.transform = `translateX(${nextPosition}px)`;
   });
  });
